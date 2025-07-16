17578eb4758c921a22e19d1e6baf86dd650407f8
trendikalahub <trendikalahub@gmail.com>
2025-07-16 23:25:58 +0530

    pattern changes

--- lib/main.dart
+++ lib/main.dart
@@ -9,145 +9,135 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
-    );
-  }
-}
-
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Container(
-          width: 500,
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 247, 246, 246),
-            borderRadius: BorderRadius.circular(12),
-            boxShadow: const [
-              BoxShadow(
-                color: Color.fromARGB(206, 0, 0, 0),
-                blurRadius: 12,
-                offset: Offset(0, 4),
-              ),
-            ],
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              //First Section
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-                child: Container(
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/logo.png',
-                        height: 80,
-                      ),
-                      const SizedBox(width: 16),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            'Heartrate',
-                            style: TextStyle(
-                              color: Color(0xFFD32F2F),
-                              fontSize: 30,
-                              fontWeight: FontWeight.w600,
-                              height: 1.0,
-                              letterSpacing: 2,
-                            ),
-                          ),
-                          Text(
-                            'HEALTHCARE',
-                            style: TextStyle(
-                                color: Color(0xFFF57C00),
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500,
-                                letterSpacing: 6,
-                                height: 1.3),
-                          ),
-                          SizedBox(
-                            height: 2,
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 4),
-                            child: Text(
-                              'KEEP A TRACK OF YOUR RHYTHM',
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Container(
+            width: 500,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 247, 246, 246),
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: const [
+                BoxShadow(
+                  color: Color.fromARGB(206, 0, 0, 0),
+                  blurRadius: 12,
+                  offset: Offset(0, 4),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                //First Section
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/logo.png',
+                          height: 80,
+                        ),
+                        const SizedBox(width: 16),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Heartrate',
                               style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 7,
-                                letterSpacing: 1,
-                                height: 1.2,
+                                color: Color(0xFFD32F2F),
+                                fontSize: 30,
+                                fontWeight: FontWeight.w600,
+                                height: 1.0,
+                                letterSpacing: 2,
                               ),
                             ),
+                            Text(
+                              'HEALTHCARE',
+                              style: TextStyle(
+                                  color: Color(0xFFF57C00),
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                  letterSpacing: 6,
+                                  height: 1.3),
+                            ),
+                            SizedBox(
+                              height: 2,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 4),
+                              child: Text(
+                                'KEEP A TRACK OF YOUR RHYTHM',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 7,
+                                  letterSpacing: 1,
+                                  height: 1.2,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                //second section
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF0E4466),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  padding: const EdgeInsets.symmetric(vertical: 24),
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Color.fromARGB(227, 0, 0, 0),
+                              blurRadius: 12,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            print('Button clicked');
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            foregroundColor: Colors.black87,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 60, vertical: 12),
                           ),
-                        ],
+                          child: Text('Login'),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        'Version :- 1.3.47',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 13,
+                        ),
                       ),
                     ],
                   ),
                 ),
-              ),
-
-              //second section
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Color(0xFF0E4466),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                padding: const EdgeInsets.symmetric(vertical: 24),
-                child: Column(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Color.fromARGB(227, 0, 0, 0),
-                            blurRadius: 12,
-                            offset: Offset(0, 3),
-                          ),
-                        ],
-                      ),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          print('Button clicked');
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.white,
-                          foregroundColor: Colors.black87,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 60, vertical: 12),
-                        ),
-                        child: const Text('Login'),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    const Text(
-                      'Version :- 1.3.47',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 13,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
